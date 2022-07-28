787333f2a8de105fac27b1bdd33a3058a49e9a8f
Micaela H. Cánova <107763342+MhCanova@users.noreply.github.com>
2022-07-28 14:15:03 -0300

    Update Oscilador.cs

--- Oscilador.cs
+++ Oscilador.cs
@@ -4,27 +4,20 @@ using UnityEngine;
 
 public class Oscilador : MonoBehaviour
 {
-    Vector3 startingPosition;
-    [SerializeField] Vector3 movementVector;
-    float movementFactor;
-    [SerializeField] float period = 2f;
-    void Start()
-    {
-        startingPosition = transform.position;
-        
-    }
+    [SerializeField] Vector3 startingPosition;
+    [SerializeField] Vector3 endPosition;
+    [SerializeField] [Range (0,1)] float speed;
+    float time = 0f;
 
     void Update()
     {
-        if (period <= Mathf.Epsilon) {return;} 
-        float cycles = Time.time / period; // continua creciendo con el tiempo
-        
-        const float tau = Mathf.PI * 2; // valor constante de 6.283
-        float rawSinWave = Mathf.Sin(cycles * tau); // de -1 a 1
+        Move();
+    }
 
-        movementFactor = (rawSinWave +1f) / 2f; // recalculado para ir de 0 a 1
-        
-        Vector3 offset = movementVector * movementFactor;
-        transform.position = startingPosition + offset;
+    private void Move()
+    {
+        time = time + Time.deltaTime * speed;
+        var pingPong = Mathf.PingPong(time, 1);
+        transform.position = Vector3.Lerp(startingPosition, endPosition, pingPong);
     }
 }
